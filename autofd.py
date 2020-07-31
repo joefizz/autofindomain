6c811043ca63b6e4bea68dd5ff4982f3b1da7db1
joefizz <joseph.rapley@hotmail.com>
2020-07-31 08:04:50 +0100

    Update autofd.py

--- autofd.py
+++ autofd.py
@@ -382,8 +382,10 @@ def toSlack(program):
 				proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 				data = {'initial_comment':'New subdomain discovered for '+program+': '+url+' - pointing to '+str(IP),'channels':slack_channel}
 				headers = {'Authorization':'Bearer '+slack_oauth_token}
-				r =requests.post(slack_api+'files.upload', data, headers=headers, files={"file": (aquatone_web_path+'/'+program+'/'+screenshotPath, open(aquatone_web_path+'/'+program+'/'+screenshotPath, "rb"), "image/png")})
-
+				try:
+					r =requests.post(slack_api+'files.upload', data, headers=headers, files={"file": (aquatone_web_path+'/'+program+'/'+screenshotPath, open(aquatone_web_path+'/'+program+'/'+screenshotPath, "rb"), "image/png")})
+				except Exception as e:
+					raise e
 
 def main():
 
@@ -460,7 +462,7 @@ def main():
 				else:
 					for line in exlist:
 						if line.rstrip('\n') == program.rstrip('\n'):
-							print(tbad,'**** Program %s is in exclude list, skipping',tend)
+							print(tbad,'**** Program %s is in exclude list, skipping'%(program),tend)
 							exclude = True
 				if exclude:
 					continue
