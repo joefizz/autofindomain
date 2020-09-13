14111b9f454944ea3b529f1dd433a162d61ab72a
joefizz <joseph.rapley@hotmail.com>
2020-09-13 22:52:06 +0100

    Update autofd.py

--- autofd.py
+++ autofd.py
@@ -490,10 +490,10 @@ def nuclei(program, linux):
 
 	if linux == 'true':
 		print('nuclei linux')
-		os.system('cat ./programs/'+program+'/report.txt | ./nuclei/linux/nuclei -t ./nuclei/nuclei-templates/technologies/ -t ./nuclei/nuclei-templates/vulnerabilities/ -t ./nuclei/nuclei-templates/default-credentials/ -t ./nuclei/nuclei-templates/subdomain-takeover/ -t ./nuclei/nuclei-templates/cves/ -t ./nuclei/nuclei-templates/files/ -o ./programs/'+program+'/nuclei-out-'+timestamp+'.txt -silent')
+		os.system('cat ./programs/'+program+'/report.txt | ./nuclei/linux/nuclei -t ./nuclei/nuclei-templates/technologies/ -t ./nuclei/nuclei-templates/vulnerabilities/ -t ./nuclei/nuclei-templates/default-credentials/ -t ./nuclei/nuclei-templates/subdomain-takeover/ -t ./nuclei/nuclei-templates/cves/ -t ./nuclei/nuclei-templates/files/ -o ./programs/'+program+'/nuclei-out-'+timestamp+'.txt')
 	else:
 		print('nuclei mac')
-		os.system('cat ./programs/'+program+'/report.txt | ./nuclei/mac/nuclei -t ./nuclei/nuclei-templates/technologies/ -t ./nuclei/nuclei-templates/vulnerabilities/ -t ./nuclei/nuclei-templates/default-credentials/ -t ./nuclei/nuclei-templates/subdomain-takeover/ -t ./nuclei/nuclei-templates/cves/ -t ./nuclei/nuclei-templates/files/ -o ./programs/'+program+'/nuclei-out-'+timestamp+'.txt -silent')
+		os.system('cat ./programs/'+program+'/report.txt | ./nuclei/mac/nuclei -t ./nuclei/nuclei-templates/technologies/ -t ./nuclei/nuclei-templates/vulnerabilities/ -t ./nuclei/nuclei-templates/default-credentials/ -t ./nuclei/nuclei-templates/subdomain-takeover/ -t ./nuclei/nuclei-templates/cves/ -t ./nuclei/nuclei-templates/files/ -o ./programs/'+program+'/nuclei-out-'+timestamp+'.txt')
 
 def toSlack(program):
 	print (tgood,"Sending latest data for %s to slack"%(program),tend)
